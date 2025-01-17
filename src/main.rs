22d613222ed2ff03acf63cdcb80d9d2c5d319de1
etiennecollin <collin.etienne.contact@gmail.com>
2025-01-16 22:25:22 -0500

    Added features for log vs defmt

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 #![no_main]
 #![feature(impl_trait_in_assoc_type)]
 
-use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::{
@@ -12,31 +11,33 @@ use esp_hal::{
 
 use {esp_backtrace as _, esp_println as _};
 
+#[cfg(feature = "defmt")]
+use defmt::info;
+#[cfg(feature = "log")]
+use log::info;
+
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    let peripherals = esp_hal::init(esp_hal::Config::default());
+    #[cfg(feature = "log")]
+    esp_println::logger::init_logger_from_env();
 
+    let peripherals = esp_hal::init(esp_hal::Config::default());
     let timg0 = TimerGroup::new(peripherals.TIMG0);
+
     esp_hal_embassy::init(timg0.timer0);
     info!("Embassy initialized!");
+    let led = Output::new(peripherals.GPIO20, Level::Low);
 
     panic!("VERIFY THE GPIO PIN NUMBER IS CONNECTED TO A LED BEFORE RUNNING THIS EXAMPLE!");
-    let led = Output::new(peripherals.GPIO20, Level::Low);
     spawner.spawn(blinky(led)).ok();
-
     spawner.spawn(greet()).ok();
-
-    loop {
-        info!("bing!");
-        Timer::after(Duration::from_millis(5_000)).await;
-    }
 }
 
 #[embassy_executor::task]
 async fn blinky(mut led: esp_hal::gpio::Output<'static>) {
     loop {
         led.toggle();
-        Timer::after(Duration::from_millis(100)).await;
+        Timer::after(Duration::from_millis(3_000)).await;
     }
 }
 
